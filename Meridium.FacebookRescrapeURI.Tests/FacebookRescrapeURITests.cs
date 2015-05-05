5dd45bb18c2cc414ad55a057842a9f78f0119a6a
Kim Gunnarsson <kim.gunnarsson@gmail.com>
2015-05-05 17:27:22 +0200

    Added Unit tests
    Not great code coverage, but it exists.
    Fixes #1

---
+++ Meridium.FacebookRescrapeURI.Tests/FacebookRescrapeURITests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Meridium.FacebookRescrapeURI.Tests
+{
+	[TestFixture]
+	public class FacebookRescrapeURITests
+	{
+		[Test]
+		public void Assert_RescrapeObject_IsValid()
+		{
+			var rescrapeObject = Rescraper.CreateRescrapableObject("http://www.meridium.se");
+			Assert.IsTrue(rescrapeObject.IsValidForScraping());
+		}
+
+		[Test]
+		public void Assert_RescrapeObject_IsInValid()
+		{
+			var rescrapeObject = Rescraper.CreateRescrapableObject(null);
+			Assert.IsFalse(rescrapeObject.IsValidForScraping());
+		}
+	}
+}
